1927c9ac85ae41a952bbea4c3b098a18fdb31179
Sienna <30266946@bsdc.ac.uk>
2024-10-23 13:13:31 +0100

    changes

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 "use strict";
 
+const name = "Sienna";
+
+if (name === "Sienna") {
+    console.log("Welcome back Sienna!")
+};
+
+let lastname = "Garfield";
+if (lastname === "Crofts") {
+    console.log("That's the correct one!")
+}
+
+else {
+    console.log("Hey, you're not the correct person!")
+}
